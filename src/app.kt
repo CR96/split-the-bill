9b64f86fa64fae429fb4a74dfe1cd79e838f60c1
Corey Rowe <coreyr1196@gmail.com>
2020-06-10 01:45:10 -0400

    Add simulated UI logic

--- src/app.kt
+++ src/app.kt
@@ -1,52 +1,85 @@
 import java.text.NumberFormat
 
+// List of people who will be splitting the bill.
+val people = arrayListOf<Person>()
+
+// List of items to be split
+val receipt = arrayListOf<Item>()
+
 fun main() {
-    // Get list of people who will be splitting the bill.
-    val aaron = Person()
-    aaron.name = "Aaron"
-
-    val corey = Person()
-    corey.name = "Corey"
-
-    val lizzi = Person()
-    lizzi.name = "Lizzi"
-
-    val people = arrayOf(aaron, corey, lizzi)
-
-    // Create items. Will be created from deserialized JSON eventually.
-    // We'll use eggs, milk, and cereal as an example.
-
-    val eggs = Item()
-    eggs.name = "Meijer Grade-A Cage Free Large Eggs, 12 Count"
-    eggs.price = 1.69
-    eggs.quantity = 1 // 1 unit (i.e. one carton of 12 eggs)
-
-    val milk = Item()
-    milk.name = "Meijer 1% Lowfat Milk, Half Gallon"
-    milk.price = 1.29
-    milk.quantity = 2
-
-    val cereal = Item()
-    cereal.name = "Honey Bunches of Oats Almond, 18 oz."
-    cereal.price = 3.59
-    cereal.quantity = 1
-
-    // Assign "owners" to each item based on their array index.
-    eggs.owners = arrayOf(0, 1, 2) // Everyone shares the eggs
-    milk.owners = arrayOf(1, 2) // The milk is split between Corey and Lizzi
-    cereal.owners = arrayOf(1) // The cereal is just for Corey
-
-    val receipt = arrayOf(eggs, milk, cereal)
+    readPerson()
+    readItem()
 
     calculateTotals(people, receipt)
 }
 
-fun calculateTotals(people: Array<Person>, receipt: Array<Item>) {
+// These two "read" functions are meant to simulate input through a GUI.
+// They ensure UI logic is entirely separated from business logic before
+// work on the Android interface begins. Time wasn't taken to sanitize
+// or validate inputs here since this project will be released as a
+// mobile application, not a command-line program.
+fun readPerson() {
+    print("Enter name of person: ")
+    val name = readLine().toString()
+
+    addPerson(name)
+
+    print("$name added. Add someone else? [y/n] ")
+
+    if (readLine() == "y") {
+        readPerson()
+    }
+}
+
+fun readItem() {
+    print("Enter item name: ")
+    val name = readLine().toString()
+
+    print("Enter cost of $name: ")
+    val price = readLine()?.toDouble()
+
+    val owners = arrayListOf<Person>()
+
+    for (person in people) {
+        print("Is ${person.name} helping pay for $name? [y/n] " )
+
+        if (readLine() == "y") {
+            owners.add(person)
+        }
+    }
+
+    if (price != null) {
+        addItem(name, price, owners)
+    }
+
+    print("$name added. Add another item? [y/n] ")
+    if (readLine() == "y") {
+        readItem()
+    }
+}
+
+fun addPerson(name: String) { // Business logic
+    val person = Person()
+    person.name = name
+
+    people.add(person)
+}
+
+fun addItem(name: String, price: Double, owners: ArrayList<Person>) {
+    val item = Item()
+    item.name = name
+    item.price = price
+    item.owners = owners
+
+    receipt.add(item)
+}
+
+fun calculateTotals(people: ArrayList<Person>, receipt: ArrayList<Item>) {
     for (item in receipt) {
         for (owner in item.owners) {
             // Split item cost evenly between owners
             // TODO: Allow for custom split (e.g. 70/30)
-            people[owner].total += (item.price / item.owners.size)
+            owner.total += (item.price / item.owners.size)
         }
     }
 
@@ -65,6 +98,5 @@ class Person {
 class Item {
     var name: String = ""
     var price: Double = 0.00
-    var quantity: Int = 1
-    var owners = arrayOf(0)
+    var owners = arrayListOf<Person>()
 }
